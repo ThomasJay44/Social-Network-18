963002a8b2b4056d97466797b10c3b6acc99c69c
tommysoprano <thomasjaymaly@gmail.com>
2023-01-30 18:35:50 -0500

    thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,32 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const { Schema, model } = require("mongoose");
 
-module.exports = { User, Thought}
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model('thought, ThoughtSchema');
+
+module.exports = Thought;
