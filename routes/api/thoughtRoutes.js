de7d742017f4526482ee29458f773e3e6cdde225
tommysoprano <thomasjaymaly@gmail.com>
2023-01-30 20:43:12 -0500

    thoughtroute

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,14 +9,14 @@ const {
   deleteReaction,
 } = require('../../controllers/thoughtController.js');
 
-// /api/courses
+// /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
-// /api/courses/:courseId
-router
-  .route('/:courseId')
-  .get(getSingleCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
 
 module.exports = router;
